2a64f37470d4e0315780a374b5441177d1bf6298
SLVladislav <bakalavrat.789@gmail.com>
2025-05-12 21:49:52 +0300

    refactory/form

--- src/2-form.js
+++ src/2-form.js
@@ -6,36 +6,45 @@ const formData = {
 const localStorageKey = "feedback-form-state";
 
 const formEl = document.querySelector(".feedback-form");
-const localValue = localStorage.getItem(localStorageKey) ?? {};
-// console.log(localValue);
+const localValue = localStorage.getItem(localStorageKey);
+if (localValue) {
+    const parseLocal = JSON.parse(localValue);
+    formEl.elements.email.value = parseLocal || '';
+    formEl.elements.message.value = parseLocal || '';
+    formData.email = parseLocal || '';
+    formData.message = parseLocal || '';
+}  
 
 
-// const localValue = localStorage.getItem(localStorageKey) ?? formData;
-// console.log(localData);
+
+
+
 
 // console.log(formEl);
 formEl.addEventListener("input", () => {    
-    const emailForm = formEl.elements.email.value;    
-    const message = formEl.elements.message.value;
+    const emailForm = formEl.elements.email.value.trim();    
+    const messageForm = formEl.elements.message.value.trim();
     formData.email = emailForm;
-    formData.message = message;
-
-    if (localStorage.getItem(localStorageKey)) {
-      console.log( JSON.parse(localStorage.getItem(localStorageKey)));      
-    } 
-    
-   
+    formData.message = messageForm;  
+    console.log(emailForm);
     
     
-//    localStorage.setItem(localStorageKey, JSON.stringify(formData));
+   localStorage.setItem(localStorageKey, JSON.stringify(formData));
     // console.log(storageForm);    
     
     
 });
 formEl.addEventListener("submit", evt => {
     evt.preventDefault();
-    localStorage.clear();
-    // localStorage.setItem(localStorageKey, evt.target.value);
+    if (!formData.email || !formData.message){
+        return console.log("Fill please all fields");
+            }
+    console.log(formData);
+    
+    localStorage.removeItem(localStorageKey);
+    formEl.reset();
+    formData.email = "";
+    formData.message = "";   
     
 })
 
